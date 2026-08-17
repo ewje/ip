536bc0ea03e8218a9622410db98ac8cfd6916520
ewje <ethanwje@gmail.com>
2026-08-17 14:02:28 +0800

    Duke.java: Changed banner, added greeting and goodbye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String banner = """
+                 ==================================================================\s
+                  ____                 \s
+                 / ___| __ _ _ __ _   _\s
+                | |  _ / _` | '__| | | |
+                | |_| | (_| | |  | |_| |
+                 \\____|\\__,_|_|   \\__, |
+                                  |___/\s
+                 ==================================================================\s
+                """;
+        String greeting = """
+                HELLO! I'm GARY!
+                How can I help you today?
+                (Type bye to exit)
+                """;
+        String goodbye = "Bye! Hope to see you again soon!\n";
+
+        System.out.println(banner + greeting + goodbye);
+
     }
 }
