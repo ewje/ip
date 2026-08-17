485f020abd549ce61540d11afd5be1fe99312e1b
ewje <ethanwje@gmail.com>
2026-08-17 14:03:32 +0800

    Duke.java: Added echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String user;
+
         String banner = """
                  ==================================================================\s
                   ____                 \s
@@ -17,7 +26,18 @@ public class Duke {
                 """;
         String goodbye = "Bye! Hope to see you again soon!\n";
 
-        System.out.println(banner + greeting + goodbye);
+        System.out.println(banner + greeting);
 
+        while (true) {
+            user = scanner.nextLine();
+
+            if (user.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            }
+            printLine();
+            System.out.println("User Input: " + user);
+            printLine();
+        }
     }
 }
